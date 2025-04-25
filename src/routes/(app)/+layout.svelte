6bc59472df9531b127243e4e4f328a981b80d5e0
Laurenn Jean <laurenn.jean@intrasense.fr>
2025-04-25 09:40:18 +0200

    Deleted the ChangeLogModal that pops up when Open WebUI starts up

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -41,7 +41,6 @@
 
 	import Sidebar from '$lib/components/layout/Sidebar.svelte';
 	import SettingsModal from '$lib/components/chat/SettingsModal.svelte';
-	import ChangelogModal from '$lib/components/ChangelogModal.svelte';
 	import AccountPending from '$lib/components/layout/Overlay/AccountPending.svelte';
 	import UpdateInfoToast from '$lib/components/layout/UpdateInfoToast.svelte';
 	import { get } from 'svelte/store';
@@ -236,7 +235,6 @@
 </script>
 
 <SettingsModal bind:show={$showSettings} />
-<ChangelogModal bind:show={$showChangelog} />
 
 {#if version && compareVersion(version.latest, version.current) && ($settings?.showUpdateToast ?? true)}
 	<div class=" absolute bottom-8 right-8 z-50" in:fade={{ duration: 100 }}>
