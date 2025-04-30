7ce9f832e5cbae98991abf987b39937470405d0c
Laurenn Jean <laurenn.jean@intrasense.fr>
2025-04-30 13:22:03 +0200

    Modified the CORS middleware to avoid losing connexion with the backend when sign out

--- backend/open_webui/main.py
+++ backend/open_webui/main.py
@@ -889,7 +889,7 @@ async def inspect_websocket(request: Request, call_next):
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=CORS_ALLOW_ORIGIN,
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
