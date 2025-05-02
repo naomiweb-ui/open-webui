0793baede67f3fd1798937c0b7c74389143d9bf7
Laurenn Jean <laurenn.jean@intrasense.fr>
2025-05-02 09:57:16 +0200

    Deleted the New Open WebUI version toast

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -42,7 +42,6 @@
 	import Sidebar from '$lib/components/layout/Sidebar.svelte';
 	import SettingsModal from '$lib/components/chat/SettingsModal.svelte';
 	import AccountPending from '$lib/components/layout/Overlay/AccountPending.svelte';
-	import UpdateInfoToast from '$lib/components/layout/UpdateInfoToast.svelte';
 	import { get } from 'svelte/store';
 
 	const i18n = getContext('i18n');
@@ -235,18 +234,6 @@
 </script>
 
 <SettingsModal bind:show={$showSettings} />
-
-{#if version && compareVersion(version.latest, version.current) && ($settings?.showUpdateToast ?? true)}
-	<div class=" absolute bottom-8 right-8 z-50" in:fade={{ duration: 100 }}>
-		<UpdateInfoToast
-			{version}
-			on:close={() => {
-				localStorage.setItem('dismissedUpdateToast', Date.now().toString());
-				version = null;
-			}}
-		/>
-	</div>
-{/if}
 
 <div class="app relative">
 	<div
