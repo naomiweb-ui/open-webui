2e848e0e4369bc267ed139977a8580da7c0911fe
Laurenn Jean <laurenn.jean@intrasense.fr>
2025-05-05 00:12:47 +0200

    Deleted Continue Response button in chats

--- src/lib/components/chat/Messages/ResponseMessage.svelte
+++ src/lib/components/chat/Messages/ResponseMessage.svelte
@@ -1233,42 +1233,7 @@
 											</button>
 										</Tooltip>
 									{/if}
-
-									{#if isLastMessage}
-										<Tooltip content={$i18n.t('Continue Response')} placement="bottom">
-											<button
-												type="button"
-												id="continue-response-button"
-												class="{isLastMessage
-													? 'visible'
-													: 'invisible group-hover:visible'} p-1.5 hover:bg-black/5 dark:hover:bg-white/5 rounded-lg dark:hover:text-white hover:text-black transition regenerate-response-button"
-												on:click={() => {
-													continueResponse();
-												}}
-											>
-												<svg
-													xmlns="http://www.w3.org/2000/svg"
-													fill="none"
-													viewBox="0 0 24 24"
-													stroke-width="2.3"
-													stroke="currentColor"
-													class="w-4 h-4"
-												>
-													<path
-														stroke-linecap="round"
-														stroke-linejoin="round"
-														d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-													/>
-													<path
-														stroke-linecap="round"
-														stroke-linejoin="round"
-														d="M15.91 11.672a.375.375 0 0 1 0 .656l-5.603 3.113a.375.375 0 0 1-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112Z"
-													/>
-												</svg>
-											</button>
-										</Tooltip>
-									{/if}
-
+									
 									<Tooltip content={$i18n.t('Regenerate')} placement="bottom">
 										<button
 											type="button"
